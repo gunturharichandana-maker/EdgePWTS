a0a18f493dce2711e2da399d832c278726659af0
gunturharichandana <gunturharichandana@gmail.com>
2026-03-05 16:16:38 +0530

    created a framework

---
+++ Typescript/array.ts
@@ -0,0 +1,27 @@
+/*let array=[10,20,30,40, "TS","JS",true]
+console.log(array)
+array.push ("app","blu",100)
+console.log("push()",array); */ //add the element to the original array
+
+
+
+/*let array1=[10,20,30,40, "TS","JS",true]
+console.log(array1)
+array1.pop ()
+console.log("pop()",array1); */   //Remove the last element from the original array
+
+/*let array2=[10,20,30,40, "TS","JS",true]
+console.log(array2)
+array2.shift ()
+console.log("shift()",array2); */  //Remove the first element from the original array
+
+
+/*let array3=[10,20,30,40, "TS","JS",true, 'appl']
+console.log(array3)
+console.log(array3.slice(2,5)); */  //extract the portion of an element from the original array
+
+
+/*let array4=[10,20,30,40,50,60,70,"Java"]
+array4.splice (3,3, "JS","TS","MD")
+console.log(array4); */ //Adding/deleting the element from the original array
+
