a2652624a950ff4dd0e16be2ef11eadabe1edd1f
gunturharichandana-maker <gunturharichandana@gmail.com>
2026-03-25 20:42:01 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,13 +4,31 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// require('dotenv').config();
+
+// {key : value} - Object
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+
+/*
+
+1. FullParallel : true and workers : 1 - Test case will run in sequential order
+2. FullParallel : false and workers : 1 - Test case will run in sequential order
+3. FullParallel : true and workers : >1 - Test case inside the files will run in parallel but the files will run in sequential order
+4. FullParallel : false and workers : >1 - Test case inside the files will run in sequential order but the files will run in parallel
+
+
+fileName.spec.ts
+fileName.test.ts
+
+*/
+
+
+
+
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -20,17 +38,21 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  timeout : 60000,
+  reporter: [['html'], ['line'], ['allure-playwright']],
+  timeout : 10000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    screenshot :'on',
+    video :'on',
+    headless : true,
+    //viewport : {width:1280, height:720}
   },
 
   /* Configure projects for major browsers */
@@ -40,13 +62,13 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-   // {
-     //  name: 'firefox',
-     //  use: { ...devices['Desktop Firefox'] },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
     // },
 
-   // {
-     //  name: 'webkit',
+    // {
+    //   name: 'webkit',
     //   use: { ...devices['Desktop Safari'] },
     // },
 
@@ -74,7 +96,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
+  //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+}
+);
